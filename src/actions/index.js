7a2be4859374ca09839a02e78b689248ed18a01d
Alex Cho <alexsjcho@gmail.com>
2019-03-25 16:27:57 -0700

    Added app header and userpanel dropdown functionalities

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+import * as actiontypes from "./types";
+
+export const setUser = user => {
+  return {
+    type: actiontypes.SET_USER,
+    payload: {
+      currentUser: user
+    }
+  };
+};
---
+++ src/components/MetaPanel/MetaPanel.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class MetaPanel extends Component {
+  render() {
+    return <div>MetaPanel Component</div>;
+  }
+}
+
+export default MetaPanel;
