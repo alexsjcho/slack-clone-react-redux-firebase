b0158a11c6a4e65a4ddeb8b317d52e100ee9da8a
Alex Cho <alexsjcho@gmail.com>
2019-03-25 15:50:03 -0700

    added onAuthStateChanged to listenf or Login/Register

--- src/index.js
+++ src/index.js
@@ -1,23 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  withRouter
+} from "react-router-dom";
 import registerServiceWorker from "./registerServiceWorker";
 import "semantic-ui-css/semantic.min.css";
 import App from "./components/App";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
+import firebase from "./firebase";
 
-const Root = () => {
-  return (
-    <Router>
+class Root extends React.Component {
+  componentDidMount() {
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        this.props.history.push("/");
+      }
+    });
+  }
+
+  render() {
+    return (
       <Switch>
         <Route exact path="/" component={App} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
       </Switch>
-    </Router>
-  );
-};
+    );
+  }
+}
 
-ReactDOM.render(<Root />, document.getElementById("root"));
+const RootWithAuth = withRouter(Root);
+
+ReactDOM.render(
+  <Router>
+    <RootWithAuth />
+  </Router>,
+  document.getElementById("root")
+);
 registerServiceWorker();
