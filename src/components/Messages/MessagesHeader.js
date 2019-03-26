70cbfc4de619876f5f4300748b9b9cd7f25bdd21
Alex Cho <alexsjcho@gmail.com>
2019-03-26 13:49:17 -0700

    updated Starred property for user data upon starring a channel

--- src/components/Messages/MessagesHeader.js
+++ src/components/Messages/MessagesHeader.js
@@ -7,7 +7,10 @@ class MessagesHeader extends React.Component {
       channelName,
       numUniqueUsers,
       handleSearchChange,
-      searchLoading
+      searchLoading,
+      isPrivateChannel,
+      handleStar,
+      isChannelStarred
     } = this.props;
 
     return (
@@ -16,7 +19,13 @@ class MessagesHeader extends React.Component {
         <Header fluid="true" as="h2" floated="left" style={{ marginBottom: 0 }}>
           <span>
             {channelName}
-            <Icon name={"star outline"} color="black" />
+            {!isPrivateChannel && (
+              <Icon
+                onClick={handleStar}
+                name={isChannelStarred ? "star" : "star outline"}
+                color={isChannelStarred ? "yellow" : "black"}
+              />
+            )}
           </span>
           <Header.Subheader>{numUniqueUsers}</Header.Subheader>
         </Header>
