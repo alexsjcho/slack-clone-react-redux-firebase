a4691775074ac04858bb71473bad0e468e3f5b73
Alex Cho <alexsjcho@gmail.com>
2019-03-25 18:59:18 -0700

    scaffold colorpanel, message components

--- src/components/ColorPanel/ColorPanel.js
+++ src/components/ColorPanel/ColorPanel.js
@@ -1,8 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
+import { Sidebar, Menu, Divider, Button } from "semantic-ui-react";
 
-class ColorPanel extends Component {
+class ColorPanel extends React.Component {
   render() {
-    return <div>ColorPanel Component</div>;
+    return (
+      <Sidebar
+        as={Menu}
+        icon="labeled"
+        inverted
+        vertical
+        visible
+        width="very thin">
+        <Divider />
+        <Button icon="add" size="small" color="blue" />
+      </Sidebar>
+    );
   }
 }
 
--- src/components/Messages/Messages.js
+++ src/components/Messages/Messages.js
@@ -1,8 +1,22 @@
-import React, { Component } from "react";
+import React from "react";
+import { Segment, Comment } from "semantic-ui-react";
 
-class Messages extends Component {
+import MessagesHeader from "./MessagesHeader";
+import MessageForm from "./MessageForm";
+
+class Messages extends React.Component {
   render() {
-    return <div>Messages Component</div>;
+    return (
+      <React.Fragment>
+        <MessagesHeader />
+
+        <Segment>
+          <Comment.Group className="messages">{/* Messages */}</Comment.Group>
+        </Segment>
+
+        <MessageForm />
+      </React.Fragment>
+    );
   }
 }
 
